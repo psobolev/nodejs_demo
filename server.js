98439b53cfbb97168c9a40a8476d09e04c3dc154
psobolev <54080135+psobolev@users.noreply.github.com>
2019-11-27 22:18:13 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+const http = require('http')  //  подключение модуля http.
+const hostname = '127.0.0.1'  
+const port = 3000
+// создаём новый HTTP-сервер
+// req - запрос, res — ответ
+const server = http.createServer((req, res) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/plain')
+  res.end('Hello World\n')
+})
+// прослушивание определённого порта на заданном хосте
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`)
+})
