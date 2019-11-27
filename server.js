177e7f247dc992ee34e5254580c08574bbe7b730
psobolev <54080135+psobolev@users.noreply.github.com>
2019-11-27 22:28:09 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const server = http.createServer((req, res) => {
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/plain')
   res.end('Hello World\n')
-}
+})
 // прослушивание определённого порта на заданном хосте
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`)
-}
+})
 // Для проверки сервера введите в адресной строке браузера http://127.0.0.1:3000
