053e7a9d9afbb3509443ca48abbeca1faf51ca65
psobolev <54080135+psobolev@users.noreply.github.com>
2019-11-28 18:58:08 +0300

    Update fs.js

--- fs.js
+++ fs.js
@@ -7,11 +7,11 @@ fs.open('test.txt', 'r', (err, fd) => {
 // Cведения о файле содержатся в аргументе `stats`
 fs.stat('test.txt', (err, stats) => {
   if (err) {
-    console.error(err)
-    return
+    console.error(err);
+    return;
   }
   console.log('Это обычный файл? '+ stats.isFile()) //true
   console.log('Это директорий? '+ stats.isDirectory()) //false
   console.log('Это ссылка? '+ stats.isSymbolicLink()) //false
-  console.log('Размер файла? ' + stats.size) //1024000 //= 1MB
-})
+  console.log('Размер файла? ' + stats.size) //26
+});
