718518efb2e3113399d8a59c7168e0390674b73b
psobolev <54080135+psobolev@users.noreply.github.com>
2019-11-28 19:07:13 +0300

    Update path.js

--- path.js
+++ path.js
@@ -1,6 +1,7 @@
+// Загружаем стандартный модуль path для работы с путями к файлам
 const path = require('path');
 
 const test = 'test.txt';
-path.dirname(test); // /users/flavio
-path.basename(test); // test.txt
-path.extname(test); // .txt
+console.log('родительская директория файла ' + path.dirname(test)); // .
+console.log('имя файла' + path.basename(test)); // test.txt
+console.log('расширение файла' + path.extname(test)); // .txt
