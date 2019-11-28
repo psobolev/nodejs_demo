6011404a4ad4ff261fecce0f3df53df2ab848f1f
psobolev <54080135+psobolev@users.noreply.github.com>
2019-11-28 18:42:46 +0300

    Update fs.js

--- fs.js
+++ fs.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
 // Открываем файл для чтения
-fs.open('test.txt', 'r', (err, fd) => {
+fs.open('./test.txt', 'r', (err, fd) => {
   //fd - это дескриптор файла
 })
 // Получаем сведения о файле
-fs.stat('/Users/flavio/test.txt', (err, stats) => {
+fs.stat('./test.txt', (err, stats) => {
   if (err) {
-    console.error(err)
+    console.error(err);
     return
   }
   //сведения о файле содержатся в аргументе `stats`
