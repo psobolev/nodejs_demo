d397cdd07c4f5b0a62a6bef4a40142504beb1cee
psobolev <54080135+psobolev@users.noreply.github.com>
2019-11-28 19:08:09 +0300

    Update path.js

--- path.js
+++ path.js
@@ -3,5 +3,5 @@ const path = require('path');
 
 const test = 'test.txt';
 console.log('родительская директория файла ' + path.dirname(test)); // .
-console.log('имя файла' + path.basename(test)); // test.txt
-console.log('расширение файла' + path.extname(test)); // .txt
+console.log('имя файла ' + path.basename(test)); // test.txt
+console.log('расширение файла ' + path.extname(test)); // .txt
